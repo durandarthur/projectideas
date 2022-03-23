987d96c9dc093e47b2058a51cb17e17c9111ac4d
arthurdurand <durandarthur09@outlook.com>
2022-03-23 15:53:53 +0000

    started coding theme picker

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,40 @@
-import { Toolbar, Link } from "@mui/material";
-import React from "react";
-import PaletteIcon from '@mui/icons-material/Palette';
+import { Toolbar, Link, Card } from "@mui/material";
+import React, { useState } from "react";
+import PaletteIcon from "@mui/icons-material/Palette";
 import theme from "../src/theme";
 
 function Header() {
-
-  return (
-    <Toolbar sx={{ backgroundColor: theme.palette.primary.main, ">a": { pr: "20px", color: "white" } }}>
-      <Link href="/" sx={{ color: theme.palette.secondary.main+"!important", fontWeight: "bold", fontSize: "1.5rem", textDecoration: "none" }}>projectideas</Link>
-      <Link href="/submit">Post</Link>
-      <Link href="/browse">Browse</Link>
-      <Link href="/about">About</Link>
-      <PaletteIcon sx={{ color: theme.palette.secondary.main, ml: "auto" }}></PaletteIcon>
-    </Toolbar>
-  );
+	const [cardOpen, setCardOpen] = useState("none");
+	return (
+		<Toolbar
+			sx={{
+				backgroundColor: theme.palette.primary.main,
+				">a": { pr: "20px", color: "white" },
+			}}
+		>
+			<Link
+				href="/"
+				sx={{
+					color: theme.palette.secondary.main + "!important",
+					fontWeight: "bold",
+					fontSize: "1.5rem",
+					textDecoration: "none",
+				}}
+			>
+				projectideas
+			</Link>
+			<Link href="/submit">Post</Link>
+			<Link href="/browse">Browse</Link>
+			<Link href="/about">About</Link>
+			<PaletteIcon
+				onClick={() =>
+					cardOpen === "none" ? setCardOpen("flex") : setCardOpen("none")
+				}
+				sx={{ color: theme.palette.secondary.main, ml: "auto" }}
+			></PaletteIcon>
+			<Card sx={{ display: cardOpen, width: "15vw" }}>test</Card>
+		</Toolbar>
+	);
 }
 
 export default Header;
